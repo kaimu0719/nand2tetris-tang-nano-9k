e739d1c4fb727243e17e3f7f872b4f268cc91b1a
KaimuSeino <skim07a19yki@icloud.com>
2026-03-08 19:31:04 +0900

    Implement DMux4Way using three DMux instances

---
+++ 01_Boolean_Logic/14_DMux4Way/top.v
@@ -0,0 +1,23 @@
+`default_nettype none
+
+`include "../DMux4Way.v"
+
+module top(
+    input clk,
+    input btn1,
+    input btn2,
+    output [3:0] led
+);
+
+  wire a, b, c, d;
+  DMux4Way DMUX4WAY(
+    .in(1'b1),
+    .sel({~btn2, ~btn1}),
+    .a(a), .b(b), .c(c), .d(d)
+  );
+  assign led[0] = ~a;
+  assign led[1] = ~b;
+  assign led[2] = ~c;
+  assign led[3] = ~d;
+
+endmodule
---
+++ 01_Boolean_Logic/DMux4Way.v
@@ -0,0 +1,19 @@
+`default_nettype none
+
+`include "DMux.v"
+
+module DMux4Way(
+	input in,
+	input [1:0] sel,
+	output a,
+	output b,
+	output c,
+	output d
+);
+
+	wire a_or_b, c_or_d;
+	DMux DMUX_HIGH(.in(in), .sel(sel[1]), .a(a_or_b), .b(c_or_d));
+	DMux DMUX_LOW0(.in(a_or_b), .sel(sel[0]), .a(a), .b(b));
+	DMux DMUX_LOW1(.in(c_or_d), .sel(sel[0]), .a(c), .b(d));
+
+endmodule
