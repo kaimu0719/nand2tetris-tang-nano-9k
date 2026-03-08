dd221c0be4f2921cd399194c36ca828143567aa0
KaimuSeino <skim07a19yki@icloud.com>
2026-03-08 15:16:45 +0900

    Simplify Nand demo to use single LED output

--- 01_Boolean_Logic/00_Nand/top.v
+++ 01_Boolean_Logic/00_Nand/top.v
@@ -6,12 +6,11 @@ module top(
   input clk,
   input btn1,
   input btn2,
-  output [5:0] led
+  output led
 );
 
 wire out;
 Nand NAND(.a(~btn1), .b(~btn2), .out(out));
-assign led[0] = ~out;
-assign led[5:1] = 5'b11111;
+assign led = ~out;
 
 endmodule
