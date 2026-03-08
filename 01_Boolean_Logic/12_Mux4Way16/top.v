05a3b0be8b048862427c064ddb09ea57646ef88c
KaimuSeino <skim07a19yki@icloud.com>
2026-03-08 18:49:31 +0900

    Implement Mux4Way16 using three Mux16 instances

---
+++ 01_Boolean_Logic/12_Mux4Way16/top.v
@@ -0,0 +1,23 @@
+`default_nettype none
+
+`include "../Mux4Way16.v"
+
+module top(
+    input clk,
+    input btn1,
+    input btn2,
+    output [5:0] led
+);
+
+  wire [15:0] out;
+  Mux4Way16 MUX4WAY16(
+    .a(16'b0000_0000_0000_0011),
+    .b(16'b0000_0000_0000_0110),
+    .c(16'b0000_0000_0000_1100),
+    .d(16'b0000_0000_0001_1000),
+    .sel({~btn2, ~btn1}),
+    .out(out)
+  );
+  assign led = ~out[5:0];
+
+endmodule
---
+++ 01_Boolean_Logic/Mux4Way16.v
@@ -0,0 +1,19 @@
+`default_nettype none
+
+`include "Mux16.v"
+
+module Mux4Way16(
+	input [15:0] a,
+	input [15:0] b,
+	input [15:0] c,
+	input [15:0] d,
+	input [1:0] sel,
+	output [15:0] out
+);
+
+	wire [15:0] ab, cd;
+	Mux16 MUX_AB(.a(a), .b(b), .sel(sel[0]), .out(ab));
+	Mux16 MUX_CD(.a(c), .b(d), .sel(sel[0]), .out(cd));
+	Mux16 MUX_OUT(.a(ab), .b(cd), .sel(sel[1]), .out(out));
+
+endmodule
